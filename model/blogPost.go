e1b479041af4a0bc0a750b2d88dfda9cdc7b70cc
Brendan Le Glaunec <brendan.le-glaunec@epitech.eu>
2018-08-03 07:38:33 +0200

    Add blog post model validation (#23)
    Fix error message in case of unprocessable entity

--- model/blogPost.go
+++ model/blogPost.go
@@ -8,8 +8,8 @@ import (
 type BlogPost struct {
 	ID        uint      `json:"id,omitempty" gorm:"primary_key"`
 	Author    string    `json:"author,omitempty"`
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
+	Title     string    `json:"title" validate:"required"`
+	Content   string    `json:"content" validate:"required"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
 	// UpdatedAt time.Time // TODO maybe if I have time
 	// Comments   []Comment // TODO maybe if I have time
